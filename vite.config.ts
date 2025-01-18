8459a8e0cecec7b4ccdcfb8608937270ed2cc378
mobin afshari <mma137813781378@gmail.com>
2025-01-18 10:19:57 +0330

    fix: chunks decreaese a little

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr() ],
-})
+  plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) {
+              return "react-vendor";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
